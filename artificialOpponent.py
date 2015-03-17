e1001878bbefaddf7fe45933f68f7138dcdde822
Johan <johangro09@gmail.com>
2015-03-17 10:39:00 +0100

    Beginnetje voor het niet-random schieten van missiles door AI

--- artificialOpponent.py
+++ artificialOpponent.py
@@ -1,5 +1,7 @@
 """
-Documentatie volgt.
+enemyGrid is hier een lege lijst waar enkel coördinaten in worden gezet
+van missiles die zijn geschoten. Alle item zijn lists met coördinaten en
+wel of geen hit ([x, y, 0] of [x, y, 1]).
 """
 
 from collections import namedtuple
@@ -17,13 +19,8 @@ class ComputerPlayer:
         self.shipCoords = []
         self.enemyGrid = []
         
-        # Fill a list with all grid points and set them empty for the time being
-        for x in range(10):
-            for y in range(10):
-                tempList = [x, y, 0]
-                self.enemyGrid.append(tempList)
-        
         self.placeShips()
+    
     
     def placeShips(self):
         """ Zet schepen op random plekken neer """
@@ -37,6 +34,7 @@ class ComputerPlayer:
                 newShip = self.createShip(xCoord, yCoord, i, random, [])
                 
                 self.shipCoords.extend(newShip)
+    
     
     def createShip(self, xCoord, yCoord, shipSize, isHorizontal, shipCoordsList):
         """
@@ -61,46 +59,45 @@ class ComputerPlayer:
             return self.createShip(xCoord, yCoord-1, shipSize, isHorizontal, shipCoordsList)
 
         
-
-
     def shipOnCoordinate(self, coordTuple):
         """ checkt of er een schip is op de plek van het schot """
         for ship in self.shipCoords:
-            if ((ship.x == coordTuple[0]) and (ship.y == coordTuple[1])):
-                return ship.letter
+            if ((ship[0] == coordTuple[0]) and (ship[1] == coordTuple[1])):
+                return 1
                 
         return 0
     
-    def updateEnemyGrid(self, coordTuple, statusStr):
-        """ update de enemyGrid met een nieuwe status """
-        for gridItem in self.enemyGrid:
-            if (gridItem[0] == coordTuple):
-                gridItem[1] = statusStr
-
-
-def fireMissile(username, coordTuple):
-    """ Vuur een missile op een username op locatie (x, y) """
-    targetUser = username
-    missileResult = targetUser.shipOnCoordinate(coordTuple)
+    def getCurrentEnemyGrid(self):
+        return self.enemyGrid
     
-    targetUser.updateEnemyGrid(coordTuple, missileResult)
- 
-    return missileResult
+    def updateEnemyGrid(self, coordTuple, result):
+        """ update de enemyGrid met een nieuwe status """
+        self.enemyGrid.append([coordTuple[0], coordTuple[1], result)
+        
 
+def fireAIMissile(computerPlayer):
+    """ Kunstmatige Intelligentie (ofnouja, programmeerskills) """
+    currentGrid = computerPlayer.getCurrentEnemyGrid
+    
+    if (len(currentGrid) == 0): # First shot
+        xCoord = randrange(0, 10)
+        yCoord = randrange(0, 10)
+        
+        missileResult = computerPlayer.shipOnCoordinate((xCoord,yCoord))
+        computerPlayer.updateEnemyGrid((xCoord, yCoord), missileResult)
+        
+    # Iemand een leuk idee hoe we kunnen zorgen dat de computer schepen
+    # totaal de vernieling in schiet maar ook weet wanneer een ship is
+    # verwoest zodat ie dan weer random kan gaan klooien? :D lastig gek.
+            
+    
+    
+    
 
 # Om te testen:
 def main():
     player = ComputerPlayer("johan", 1)
     
-    # Print een overzichtelijke enemyGrid
-    gridList = ""
-    for x in range(10):
-        for y in range(10):
-            number = (x * 10) + y
-            gridList += str(player.enemyGrid[number][1]) + " "
-        
-        gridList += "\n"
-    
-    print(gridList)
+    print(player.shipCoords)
     
 main()
